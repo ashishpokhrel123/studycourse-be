e591902761fef735c45a29a738376291008b50cb
aashishesr <aashish@esrtech.io>
2024-02-24 19:07:52 +0545

    minor chnages

--- src/database/database-config.service.ts.ts
+++ src/database/database-config.service.ts.ts
@@ -1,76 +1,70 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { join } from 'path';
-import { SecretManagerServiceClient } from '@google-cloud/secret-manager';
+
+require('dotenv').config();
 
 class DatabaseConfigService {
-  private secretManagerClient: SecretManagerServiceClient;
+  constructor(private env: { [k: string]: string | undefined }) {}
 
-  constructor() {
-    this.secretManagerClient = new SecretManagerServiceClient();
-  }
-
-  private async getSecretValue(name: string): Promise<string> {
-    const [version] = await this.secretManagerClient.accessSecretVersion({
-      name: name,
-    });
-    return version.payload?.data?.toString() || '';
-  }
-
-private async resolveSecretReference(ref: string): Promise<string> {
-    if (ref.startsWith('$(ref:projects/')) {
-        const secretName = ref.match(/\$\(\S*\/secrets\/(\S*)\/versions\/latest\)/)[1];
-        const projectId = "288296468858";
-
-        // Ensure projectId is not empty before proceeding
-        if (!projectId) {
-            throw new Error('PROJECT_ID environment variable is not set');
-        }
-
-        return await this.getSecretValue(`projects/${projectId}/secrets/${secretName}/versions/latest`);
-    } else {
-        return ref;
+  private getValue(key: string, throwOnMissing = true): string {
+    const value = this.env[key];
+    if (!value && throwOnMissing && key !== 'POSTGRES_PASSWORD') {
+      throw new Error(`config error - missing env.${key}`);
     }
-}
 
+    return value || '';
+  }
 
-  public async getTypeOrmConfig(): Promise<TypeOrmModuleOptions> {
-    const postgresHost = await this.resolveSecretReference('$(ref:projects/${PROJECT_ID}/secrets/postgres-host/versions/latest)');
-    const postgresDatabase = await this.resolveSecretReference('$(ref:projects/${PROJECT_ID}/secrets/postgres-database/versions/latest)');
-    const postgresUser = await this.resolveSecretReference('$(ref:projects/${PROJECT_ID}/secrets/postgres-user/versions/latest)');
-    const postgresPassword = await this.resolveSecretReference('$(ref:projects/${PROJECT_ID}/secrets/postgres-password/versions/latest)');
-    const postgresPort = await this.resolveSecretReference('$(ref:projects/${PROJECT_ID}/secrets/postgres-port/versions/latest)');
+  public ensureValues(keys: string[]) {
+    keys.forEach((k) => this.getValue(k, true));
+    return this;
+  }
 
+  public getPort() {
+    return this.getValue('PORT', true);
+  }
+
+  public isProduction() {
+    const mode = this.getValue('MODE', false);
+    return mode != 'DEV';
+  }
+
+  public getTypeOrmConfig(): TypeOrmModuleOptions {
     return {
       type: 'postgres',
-      host: postgresHost,
-      port: parseInt(postgresPort),
-      username: postgresUser,
-      password: postgresPassword,
-      database: postgresDatabase,
+      host: this.getValue('POSTGRES_HOST'),
+      port: parseInt(this.getValue('POSTGRES_PORT')),
+      username: this.getValue('POSTGRES_USER'),
+      password: this.getValue('POSTGRES_PASSWORD') || '',
+      database: this.getValue('POSTGRES_DATABASE'),
       autoLoadEntities: true,
-      synchronize: true
     };
   }
 
-  public async getTypeOrmConfigForORM(): Promise<TypeOrmModuleOptions> {
-    const postgresHost = await this.resolveSecretReference('$(ref:projects/${PROJECT_ID}/secrets/postgres-host/versions/latest)');
-    const postgresDatabase = await this.resolveSecretReference('$(ref:projects/${PROJECT_ID}/secrets/postgres-database/versions/latest)');
-    const postgresUser = await this.resolveSecretReference('$(ref:projects/${PROJECT_ID}/secrets/postgres-user/versions/latest)');
-    const postgresPassword = await this.resolveSecretReference('$(ref:projects/${PROJECT_ID}/secrets/postgres-password/versions/latest)');
-     const postgresPort = await this.resolveSecretReference('$(ref:projects/${PROJECT_ID}/secrets/postgres-port/versions/latest)');
-
+  public getTypeOrmConfigForORM(): TypeOrmModuleOptions {
     return {
       type: 'postgres',
-      host: postgresHost,
-      port: parseInt(postgresPort),
-      username: postgresUser,
-      password: postgresPassword,
-      database: postgresDatabase,
+      host: this.getValue('POSTGRES_HOST'),
+      port: parseInt(this.getValue('POSTGRES_PORT')),
+      username: this.getValue('POSTGRES_USER'),
+      password: this.getValue('POSTGRES_PASSWORD'),
+      database: this.getValue('POSTGRES_DATABASE'),
       migrationsTableName: 'migrations',
       entities: [join(__dirname, '../common/entities/**/*{.ts,.js}')],
       migrations: ['libs/migrations/*.ts'],
+      
     };
   }
 }
 
-export const databaseConfigService = new DatabaseConfigService();
+const databaseConfigService = new DatabaseConfigService(
+  process.env
+).ensureValues([
+  'POSTGRES_HOST',
+  'POSTGRES_PORT',
+  'POSTGRES_USER',
+  'POSTGRES_PASSWORD',
+  'POSTGRES_DATABASE',
+]);
+
+export { databaseConfigService };
