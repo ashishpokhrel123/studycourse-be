67f96dfd7f1538c0e16fc439c4504050ee6ff608
aashishesr <aashish@esrtech.io>
2024-02-24 22:00:10 +0545

    minor chnages

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,19 +8,27 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Course } from './common/entities/course.entity';
 import { University } from './common/entities/university.entity';
-import { databaseConfigService } from './database/database-config.service.ts';
+// import { databaseConfigService } from './database/database-config.service.ts';
 import { ScUser } from './common/entities/user.entity';
 import { StudyLevelModule } from './app/study-level/module/study-level.module';
 import { SubjectModule } from './app/subject/module/subject.module';
 import { DestinationModule } from './app/destination/module/destination.module';
 import { BlogModule } from './app/blog/module/blog.module';
 import { FileUploadModule } from './app/FileUpload/file-upload-module';
+import { join } from 'path';
 
 
 @Module({
   imports: [
-    TypeOrmModule.forRootAsync({
-      useFactory: async () => await databaseConfigService.getTypeOrmConfig(),
+   TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '/cloudsql/calm-edge-415106:us-central1:education-site',
+      port: 5432,
+      username: "postgres",
+      password: ">yL2*Ppxak_/QHD#",
+      database: "study-courses",
+      entities: [join(__dirname, '**/**.entity{.ts,.js}')],
+      synchronize: true,
     }),
     AuthModule,
     UserModule,
--- src/database/database-config.service.ts.ts
+++ src/database/database-config.service.ts.ts
@@ -1,89 +1,89 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { join } from 'path';
+// import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+// import { join } from 'path';
 
-require('dotenv').config();
+// require('dotenv').config();
 
-class DatabaseConfigService {
-  constructor(private env: { [k: string]: string | undefined }) {}
+// class DatabaseConfigService {
+//   constructor(private env: { [k: string]: string | undefined }) {}
 
-  private getValue(key: string, throwOnMissing = true): string {
-    const value = this.env[key];
-    if (!value && throwOnMissing && key !== 'POSTGRES_PASSWORD') {
-      throw new Error(`config error - missing env.${key}`);
-    }
+//   private getValue(key: string, throwOnMissing = true): string {
+//     const value = this.env[key];
+//     if (!value && throwOnMissing && key !== 'POSTGRES_PASSWORD') {
+//       throw new Error(`config error - missing env.${key}`);
+//     }
 
-    return value || '';
-  }
+//     return value || '';
+//   }
 
-  public ensureValues(keys: string[]) {
-    keys.forEach((k) => this.getValue(k, true));
-    return this;
-  }
+//   public ensureValues(keys: string[]) {
+//     keys.forEach((k) => this.getValue(k, true));
+//     return this;
+//   }
 
-  public getPort() {
-    return this.getValue('PORT', true);
-  }
+//   public getPort() {
+//     return this.getValue('PORT', true);
+//   }
 
-  public isProduction() {
-    const mode = this.getValue('MODE', false);
-    return mode != 'DEV';
-  }
+//   public isProduction() {
+//     const mode = this.getValue('MODE', false);
+//     return mode != 'DEV';
+//   }
 
-  public getTypeOrmConfig(): TypeOrmModuleOptions {
-    if (this.isProduction()) {
-      return this.getProductionTypeOrmConfig();
-    } else {
-      return this.getDevelopmentTypeOrmConfig();
-    }
-  }
+//   public getTypeOrmConfig(): TypeOrmModuleOptions {
+//     if (this.isProduction()) {
+//       return this.getProductionTypeOrmConfig();
+//     } else {
+//       return this.getDevelopmentTypeOrmConfig();
+//     }
+//   }
 
-  private getProductionTypeOrmConfig(): TypeOrmModuleOptions {
-    return {
-      type: 'postgres',
-      host: process.env.GAE_DB_ADDRESS || '',
-      port: parseInt(this.getValue('POSTGRES_PORT')),
-      username: this.getValue('POSTGRES_USER'),
-      password: this.getValue('POSTGRES_PASSWORD') || '',
-      database: this.getValue('POSTGRES_DATABASE'),
-      autoLoadEntities: true,
-    };
-  }
+//   private getProductionTypeOrmConfig(): TypeOrmModuleOptions {
+//     return {
+//       type: 'postgres',
+//       host: process.env.GAE_DB_ADDRESS || '',
+//       port: parseInt(this.getValue('POSTGRES_PORT')),
+//       username: this.getValue('POSTGRES_USER'),
+//       password: this.getValue('POSTGRES_PASSWORD') || '',
+//       database: this.getValue('POSTGRES_DATABASE'),
+//       autoLoadEntities: true,
+//     };
+//   }
 
-  private getDevelopmentTypeOrmConfig(): TypeOrmModuleOptions {
-    return {
-      type: 'postgres',
-      host: '127.0.0.1',
-      port: parseInt(this.getValue('POSTGRES_PORT')),
-      username: this.getValue('POSTGRES_USER'),
-      password: this.getValue('POSTGRES_PASSWORD') || '',
-      database: this.getValue('POSTGRES_DATABASE'),
-      autoLoadEntities: true,
-    };
-  }
+//   private getDevelopmentTypeOrmConfig(): TypeOrmModuleOptions {
+//     return {
+//       type: 'postgres',
+//       host: '127.0.0.1',
+//       port: parseInt(this.getValue('POSTGRES_PORT')),
+//       username: this.getValue('POSTGRES_USER'),
+//       password: this.getValue('POSTGRES_PASSWORD') || '',
+//       database: this.getValue('POSTGRES_DATABASE'),
+//       autoLoadEntities: true,
+//     };
+//   }
 
-  public getTypeOrmConfigForORM(): TypeOrmModuleOptions {
-    return {
-      type: 'postgres',
-      host: this.getValue('POSTGRES_HOST'),
-      port: parseInt(this.getValue('POSTGRES_PORT')),
-      username: this.getValue('POSTGRES_USER'),
-      password: this.getValue('POSTGRES_PASSWORD'),
-      database: this.getValue('POSTGRES_DATABASE'),
-      migrationsTableName: 'migrations',
-      entities: [join(__dirname, '../common/entities/**/*{.ts,.js}')],
-      migrations: ['libs/migrations/*.ts'],
-    };
-  }
-}
+//   public getTypeOrmConfigForORM(): TypeOrmModuleOptions {
+//     return {
+//       type: 'postgres',
+//       host: this.getValue('POSTGRES_HOST'),
+//       port: parseInt(this.getValue('POSTGRES_PORT')),
+//       username: this.getValue('POSTGRES_USER'),
+//       password: this.getValue('POSTGRES_PASSWORD'),
+//       database: this.getValue('POSTGRES_DATABASE'),
+//       migrationsTableName: 'migrations',
+//       entities: [join(__dirname, '../common/entities/**/*{.ts,.js}')],
+//       migrations: ['libs/migrations/*.ts'],
+//     };
+//   }
+// }
 
-const databaseConfigService = new DatabaseConfigService(
-  process.env
-).ensureValues([
-  'POSTGRES_HOST',
-  'POSTGRES_PORT',
-  'POSTGRES_USER',
-  'POSTGRES_PASSWORD',
-  'POSTGRES_DATABASE',
-]);
+// const databaseConfigService = new DatabaseConfigService(
+//   process.env
+// ).ensureValues([
+//   'POSTGRES_HOST',
+//   'POSTGRES_PORT',
+//   'POSTGRES_USER',
+//   'POSTGRES_PASSWORD',
+//   'POSTGRES_DATABASE',
+// ]);
 
-export { databaseConfigService };
+// export { databaseConfigService };
